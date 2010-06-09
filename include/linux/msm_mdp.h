16479dd08f062fb436d8567c6e0040b529445761
drakaz <drakpub@gmail.com>
2010-06-09 17:45:47 +0200

    XRGB/ARGB fix

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -49,6 +49,7 @@ enum {
 	MDP_Y_CBCR_H2V1,   /* Y and CrCb, pseduo planer w/ Cr is in MSB */
 	MDP_RGBA_8888,    /* ARGB 888 */
 	MDP_BGRA_8888,	  /* ABGR 888 */
+	MDP_RGBX_8888,    // RGBX 888
 	MDP_IMGTYPE_LIMIT,
 	MDP_BGR_565 = MDP_IMGTYPE2_START,      /* BGR 565 planer */
 	MDP_FB_FORMAT,    /* framebuffer format */
